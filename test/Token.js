fcf297c396dba129a57b5f920426d29724bbe5ee
Victor Inojosa <vijoin@gmail.com>
2021-12-06 10:14:24 -0300

    [TEST] Should transfer tokens between accounts Chapter 5

--- test/Token.js
+++ test/Token.js
@@ -11,3 +11,23 @@ describe("Token contract", function () {
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
     });
 });
+
+describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+        const [owner, addr1, addr2] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("Token");
+        const hardhatToken = await Token.deploy();
+
+        //Transfer 50 tokens from owner to addr1
+        await hardhatToken.transfer(addr1.address, 50);
+        expect(await hardhatToken.balanceOf(addr1.address)).to  .equal(50);
+
+        //Transfer 50 tokens from addr1 to addr2
+        await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+        expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+
+        //Remaining balance
+        expect(await hardhatToken.balanceOf(owner.address)).to.equal(999950);
+        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(0);
+    });
+});
