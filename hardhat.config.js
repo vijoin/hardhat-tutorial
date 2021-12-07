e258f8a5397ed9a81054c30a067ead48b1e1c898
Victor Inojosa <vijoin@gmail.com>
2021-12-07 08:47:23 -0300

    Include network credentials and settings for live network deployment on Ropsten Chapter 7

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,16 @@ require("@nomiclabs/hardhat-waffle");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const ALCHEMY_API_KEY = "KEY"; // get it in alchemy.com
+const ROPSTEN_PRIVATE_KEY = "PRIVKEY"; // get in metamask
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
